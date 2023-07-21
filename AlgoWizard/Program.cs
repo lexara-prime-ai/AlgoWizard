9397c4c4d5427da15a0ddec0dff9a0e4af6aaffc
irfanghat <irfanghat@gmail.com>
2023-07-21 17:37:42 +0300

    Source files

---
+++ AlgoWizard/Program.cs
@@ -0,0 +1,27 @@
+﻿using CommandLine;
+using System.Text.Json;
+using AlgoWizardCLI.Model;
+
+namespace AlgoWizardCLI 
+{
+    class Program {
+        public class Options
+        {
+            [Option('g', "generate", Required = false, HelpText = "Generate algorithm")]
+            public bool Algorithm { get; set; }
+        }
+
+        static void Main(string[] args)
+        {
+            Parser.Default.ParseArguments<Options>(args)
+                .WithParsed<Options>(o =>
+                {
+                    if (o.Algorithm)
+                    {
+                        HttpClient client = new HttpClient();
+                        
+                    }
+                });
+        }
+    }
+}
